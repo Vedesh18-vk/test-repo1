9863ca1abfeb27959a406a606eb808e360d35cf0
dev1 <dev1@gmail.com>
2026-05-01 13:14:32 +0530

    2 files by dev2

---
+++ hello_LOCAL_1206.java
@@ -0,0 +1,7 @@
+public class Main #dev2
+{
+	public static void main() #dev2
+	{
+		System.out.println("Bannu"`)
+	}
+} # dev2
---
+++ hello_REMOTE_1206.java
@@ -0,0 +1,7 @@
+public class Main #dev1
+{
+	public static void main()  #dev1
+	{
+		System.out.println("Bannu"`)
+	}    #dev1
+}
